dfe1740d9a83442ab718e28eefc60988f4a2cd1d
Muneeb-hub411 <muneebsaleem402@gmail.com>
2024-05-18 01:30:34 +0500

    added logic of password generating

--- App.tsx
+++ App.tsx
@@ -1,7 +1,44 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import * as yup from 'Yup';
+let passlenght = yup.object({
+  passlen: yup
+    .number()
+    .required()
+    .min(6, 'lenght minimum should be 6')
+    .max(10, 'lenght should be maximum of 10'),
+});
 
 export default function App() {
+  const [password, setpassword] = useState('');
+  const [upercase, setupercase] = useState('');
+  const [lowercase, setlowercase] = useState('');
+  const [specialcharacter, setspecialcharacter] = useState('');
+  let generatepass = () => {
+    let loweralphabets = 'abcdefghijklmnopqrstuvwxyz';
+    let upperalphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let specialcharacters = '%$#@^&*(';
+    let character = '';
+    if (upercase) {
+      character += upperalphabets;
+    }
+    if (lowercase) {
+      character += loweralphabets;
+    }
+    if (specialcharacter) {
+      character += specialcharacters;
+    }
+    let newpass = createpass(character, 8);
+    setpassword(newpass);
+  };
+  let createpass = (character: string, passlenght: number) => {
+    let pass = '';
+    for (let index = 0; index < passlenght; index++) {
+      let ranchar = Math.floor(Math.random() * character.length);
+      pass += character.charAt(ranchar);
+    }
+    return pass;
+  };
   return (
     <View>
       <Text>App</Text>
