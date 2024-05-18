8f43be7bc6110f63f64d50a85d210ce1d037bfb6
Muneeb-hub411 <muneebsaleem402@gmail.com>
2024-05-19 00:43:19 +0500

    Added a new checkboxes and done some styling

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import React, {useState} from 'react';
-import * as yup from 'Yup';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import * as yup from 'yup';
 let passlenght = yup.object({
   passlen: yup
     .number()
@@ -11,9 +18,9 @@ let passlenght = yup.object({
 
 export default function App() {
   const [password, setpassword] = useState('');
-  const [upercase, setupercase] = useState('');
-  const [lowercase, setlowercase] = useState('');
-  const [specialcharacter, setspecialcharacter] = useState('');
+  const [upercase, setupercase] = useState(true);
+  const [lowercase, setlowercase] = useState(true);
+  const [specialcharacter, setspecialcharacter] = useState(true);
   let generatepass = () => {
     let loweralphabets = 'abcdefghijklmnopqrstuvwxyz';
     let upperalphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -30,6 +37,7 @@ export default function App() {
     }
     let newpass = createpass(character, 8);
     setpassword(newpass);
+    console.log(newpass);
   };
   let createpass = (character: string, passlenght: number) => {
     let pass = '';
@@ -39,11 +47,85 @@ export default function App() {
     }
     return pass;
   };
+  let resetpass = () => {
+    setpassword('');
+    setlowercase(false);
+    setspecialcharacter(false);
+    setupercase(false);
+  };
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollViewContent}>
+      <View style={styles.container}>
+        <Text style={styles.text} selectable={true}>
+          Password: {password}
+        </Text>
+
+        <BouncyCheckbox
+          isChecked={upercase}
+          onPress={(isChecked: boolean) => setupercase(isChecked)}
+          text="Uppercase"
+        />
+        <BouncyCheckbox
+          isChecked={lowercase}
+          onPress={(isChecked: boolean) => setlowercase(isChecked)}
+          text="Lowercase"
+        />
+        <BouncyCheckbox
+          isChecked={specialcharacter}
+          onPress={(isChecked: boolean) => setspecialcharacter(isChecked)}
+          text="Special Characters"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={generatepass}>
+          <Text style={styles.buttonText}>Generate Password</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button2} onPress={resetpass}>
+          <Text style={styles.buttonText2}>Reset</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    fontWeight: 'condensedBold',
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    // alignItems: 'center',
+    // marginBottom: 20,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  button2: {
+    backgroundColor: '#E8290B',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText2: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
